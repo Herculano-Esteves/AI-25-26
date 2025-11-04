e217df72fee8b2aa57c0ac0afc64c319b6553e12
Herculano Esteves <pedroesteves005@gmail.com>
2025-11-04 18:07:11 +0000

    feat: simulation loop with first in first served request

--- mapGen.py
+++ mapGen.py
@@ -5,6 +5,85 @@ from nodo import Node
 from models import Veiculo, Pedido, Motorizacao
 from typing import Tuple, List
 
+def criar_mapa_gerado(
+    largura=10, altura=10, prob_posto_gas=0.1, prob_estacao_ev=0.1
+) -> Tuple[GrafoCidade, List[Veiculo], List[Pedido]]:
+    mapa = GrafoCidade()
+    nos_criados = {}
+
+    # Create nodes
+    for x in range(largura):
+        for y in range(altura):
+            gas_pumps = 0
+            energy_chargers = 0
+
+            if random.random() < prob_posto_gas:
+                gas_pumps = random.randint(2, 6)
+
+            if random.random() < prob_estacao_ev:
+                energy_chargers = random.randint(2, 6)
+
+            pos = (x, y)
+            no = Node(pos, gas_pumps, energy_chargers, random.randint(200, 900))
+            nos_criados[pos] = no
+            mapa.adicionar_no(no)
+
+    # Creates streets
+    G_grid = nx.grid_2d_graph(largura, altura)
+
+    for u_pos, v_pos in G_grid.edges():
+        no_origem = nos_criados[u_pos]
+        no_destino = nos_criados[v_pos]
+
+        distancia = 1.0
+        tempo = random.uniform(1.5, 3.0)
+
+        mapa.adicionar_aresta(no_origem, no_destino, distancia, tempo, True)
+
+    todos_os_nos = list(nos_criados.values())
+    garantir_ponto_interesse(todos_os_nos, Motorizacao.ELETRICO)
+    garantir_ponto_interesse(todos_os_nos, Motorizacao.COMBUSTAO)
+
+    veiculos = []
+    # Generates eletric vehicles
+    for i in range(3):
+        loc = random.choice(todos_os_nos)
+        veiculos.append(
+            Veiculo(
+                f"V-EV{i+1}",
+                Motorizacao.ELETRICO,
+                loc,
+                random.randint(3, 7),
+                0.035,
+                random.randint(350, 650),
+                random.randint(50, 650),
+            )
+        )
+
+    # Generates combustion vehicles
+    for i in range(3):
+        loc = random.choice(todos_os_nos)
+        veiculos.append(
+            Veiculo(
+                f"V-GAS{i+1}",
+                Motorizacao.COMBUSTAO,
+                loc,
+                random.randint(3, 7),
+                0.098,
+                random.randint(600, 900),
+                random.randint(50, 900),
+            )
+        )
+
+    # Generates random requests
+    pedidos = []
+    for i in range(5):
+        pedidos.append(criar_pedido_aleatorio(todos_os_nos))
+
+    print(
+        f"Mapa gerado: {largura}x{altura} ({len(todos_os_nos)} nós). {len(veiculos)} veículos e {len(pedidos)} pedidos criados."
+    )
+    return mapa, veiculos, pedidos
 
 def garantir_ponto_interesse(todos_os_nos, tipo_ponto):
     if tipo_ponto == Motorizacao.ELETRICO:
@@ -20,90 +99,6 @@ def garantir_ponto_interesse(todos_os_nos, tipo_ponto):
         # Converte um nó aleatório
         no_a_converter = random.choice(todos_os_nos)
         no_a_converter.gas_pumps = random.randint(2, 4)
-
-
-def criar_mapa_gerado(
-    largura=10, altura=10, prob_posto_gas=0.1, prob_estacao_ev=0.1
-) -> Tuple[GrafoCidade, List[Veiculo], List[Pedido]]:
-    mapa = GrafoCidade()
-    nos_criados = {}
-
-    # Criar nós
-    for x in range(largura):
-        for y in range(altura):
-            # Decide aleatoriamente se este nó é especial
-            gas_pumps = 0
-            energy_chargers = 0
-
-            if random.random() < prob_posto_gas:
-                gas_pumps = random.randint(2, 6)
-
-            if random.random() < prob_estacao_ev:
-                energy_chargers = random.randint(2, 6)
-
-            pos = (x, y)
-            no = Node(pos, gas_pumps, energy_chargers, random.randint(200, 900))
-            nos_criados[pos] = no
-            mapa.adicionar_no(no)
-
-    # Criar ruas
-    G_grid = nx.grid_2d_graph(largura, altura)
-
-    for u_pos, v_pos in G_grid.edges():
-        no_origem = nos_criados[u_pos]
-        no_destino = nos_criados[v_pos]
-
-        distancia = 1.0
-        tempo = random.uniform(1.5, 3.0)
-
-        mapa.adicionar_aresta(no_origem, no_destino, distancia, tempo, True)
-
-    # Criar Veículos
-    todos_os_nos = list(nos_criados.values())
-    garantir_ponto_interesse(todos_os_nos, Motorizacao.ELETRICO)
-    garantir_ponto_interesse(todos_os_nos, Motorizacao.COMBUSTAO)
-
-    veiculos = []
-    # Cria 3 veículos elétricos
-    for i in range(3):
-        loc = random.choice(todos_os_nos)
-        veiculos.append(
-            Veiculo(
-                f"V-EV{i+1}",
-                Motorizacao.ELETRICO,
-                loc,
-                random.randint(3, 7),
-                0.035,
-                random.randint(350, 650),
-                random.randint(50, 650),
-            )
-        )
-
-    # Cria 3 veículos a combustão
-    for i in range(3):
-        loc = random.choice(todos_os_nos)
-        veiculos.append(
-            Veiculo(
-                f"V-GAS{i+1}",
-                Motorizacao.COMBUSTAO,
-                loc,
-                random.randint(3, 7),
-                0.098,
-                random.randint(600, 900),
-                random.randint(50, 900),
-            )
-        )
-
-    # Criar 5 Pedidos Aleatórios
-    pedidos = []
-    for i in range(5):
-        pedidos.append(criar_pedido_aleatorio(todos_os_nos))
-
-    print(
-        f"Mapa gerado: {largura}x{altura} ({len(todos_os_nos)} nós). {len(veiculos)} veículos e {len(pedidos)} pedidos criados."
-    )
-    return mapa, veiculos, pedidos
-
 
 def criar_pedido_aleatorio(nos: List[Node]) -> Pedido:
     origem = random.choice(nos)
---
+++ simulator.py
@@ -0,0 +1,128 @@
+from mapGen import criar_mapa_gerado, criar_pedido_aleatorio
+from search import procurar_rota_a_star
+from models import EstadoVeiculo
+
+class Simulador:
+    # Simulation Constants
+    LARGURA_MAPA = 20
+    ALTURA_MAPA = 20
+    SIM_TEMPO_POR_TICK = 0.1  # Time in minutes per frame
+
+    def __init__(self):
+        # Variables
+        self.mapa = None
+        self.veiculos = []
+        self.pedidos = []
+        self.pedidos_a_ir_buscar = []
+        self.pedidos_destinos = []
+        
+        # Setup
+        self.configurar_novo_mapa()
+
+    def configurar_novo_mapa(self):
+        print("A criar novo mapa...")
+        self.mapa, self.veiculos, self.pedidos = criar_mapa_gerado(
+            self.LARGURA_MAPA, self.ALTURA_MAPA, 0.02, 0.005
+        )
+        self.pedidos_a_ir_buscar = []
+        self.pedidos_destinos = []
+
+
+    def passo_simulacao(self):
+        tempo_a_avancar = self.SIM_TEMPO_POR_TICK
+
+        for v in self.veiculos:
+            # In movement
+            if v.nodo_destino:
+                v.tempo_viagem_passado += tempo_a_avancar
+
+                if v.tempo_viagem_passado >= v.tempo_viagem_total:
+                    # Arrived at destination
+                    v.nodo_atual = v.nodo_destino
+                    v.nodo_origem = None
+                    v.nodo_destino = None
+                    v.tempo_viagem_passado = 0
+                    v.localizacao_atual_coords = v.nodo_atual.position
+
+                else:
+                    # On the way
+                    progresso = v.tempo_viagem_passado / v.tempo_viagem_total
+                    x1, y1 = v.nodo_origem.position
+                    x2, y2 = v.nodo_destino.position
+                    novo_x = x1 + (x2 - x1) * progresso
+                    novo_y = y1 + (y2 - y1) * progresso
+                    v.localizacao_atual_coords = (novo_x, novo_y)
+                    continue # Skips to next vehicle
+            
+            # With route assigned but stopped
+            if v.rota_atribuida:
+                if v.rota_atribuida[0] != v.nodo_atual:
+                    v.rota_atribuida = [] # Invalid route
+
+                elif len(v.rota_atribuida) >= 2:
+                    # Will start moving to next node
+                    proximo_no = v.rota_atribuida[1]
+                    v.nodo_origem = v.nodo_atual
+                    v.nodo_destino = proximo_no
+                    v.rota_atribuida = v.rota_atribuida[1:]
+
+                    aresta_info = self.mapa.obter_peso_aresta(
+                        v.nodo_origem, v.nodo_destino
+                    )
+                    if aresta_info:
+                        distancia, tempo = aresta_info
+                        v.tempo_viagem_total = tempo
+                        v.tempo_viagem_passado = 0.0
+                    continue # Skips to next vehicle
+
+                else:
+                    v.rota_atribuida = [] # Reached destination of route
+
+            # If here there is no movement or route
+            if v.estado == EstadoVeiculo.DISPONIVEL:
+                if not self.pedidos:
+                    for _ in range(4):
+                      self.pedidos.append(criar_pedido_aleatorio(list(self.mapa.nos)))
+                    continue # No requests available
+
+                # First request in the list
+                pedido_escolhido = self.pedidos.pop(0)
+                self.pedidos_a_ir_buscar.append(pedido_escolhido)
+
+                v.pedido_atual = pedido_escolhido
+                v.estado = EstadoVeiculo.A_CAMINHO_CLIENTE
+                print(
+                    f"{v.id_veiculo} aceitou {pedido_escolhido.id_pedido}. A caminho de {pedido_escolhido.origem.position}"
+                )
+
+                caminho = procurar_rota_a_star(
+                    self.mapa, v.nodo_atual, pedido_escolhido.origem
+                )
+                v.rota_atribuida = caminho if caminho else []
+
+            elif v.estado == EstadoVeiculo.A_CAMINHO_CLIENTE:
+                # Reached client
+                print(
+                    f"{v.id_veiculo} em {v.nodo_atual.position}. A iniciar viagem para {v.pedido_atual.destino.position}"
+                )
+
+                self.pedidos_a_ir_buscar.remove(v.pedido_atual)
+                self.pedidos_destinos.append(v.pedido_atual)
+
+                v.estado = EstadoVeiculo.EM_VIAGEM_CLIENTE
+                
+                caminho = procurar_rota_a_star(
+                    self.mapa, v.nodo_atual, v.pedido_atual.destino
+                )
+                v.rota_atribuida = caminho if caminho else []
+
+            elif v.estado == EstadoVeiculo.EM_VIAGEM_CLIENTE:
+                # Just dropped off the client
+                print(
+                    f"{v.id_veiculo} completou a viagem em {v.nodo_atual.position}. Disponível."
+                )
+
+                self.pedidos_destinos.remove(v.pedido_atual)
+                v.estado = EstadoVeiculo.DISPONIVEL
+                v.pedido_atual = None
+                v.rota_atribuida = []
